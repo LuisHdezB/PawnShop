32da9c29fa5357a9a5cc5684baa234d64531500a
Alex Moreno <alx.spain@gmail.com>
2018-04-26 13:42:55 +0100

    Limpiado imports y añadido contenido de imagen

--- PawnShopApp/src/main/java/es/ulpgc/eite/clean/mvp/sample/maps/MapsView.java
+++ PawnShopApp/src/main/java/es/ulpgc/eite/clean/mvp/sample/maps/MapsView.java
@@ -1,23 +1,13 @@
 package es.ulpgc.eite.clean.mvp.sample.maps;
 
 import android.annotation.SuppressLint;
-import android.content.Context;
-import android.content.Intent;
-import android.content.SharedPreferences;
-import android.graphics.Bitmap;
-import android.graphics.Canvas;
-import android.graphics.drawable.Drawable;
-import android.graphics.drawable.DrawableContainer;
+
 import android.os.Bundle;
-import android.support.annotation.DrawableRes;
-import android.support.v4.content.ContextCompat;
-import android.support.v7.widget.Toolbar;
+
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
-import android.widget.RelativeLayout;
-import android.widget.TextView;
+
 
 
 
@@ -26,24 +16,18 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.MapsInitializer;
-import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.BitmapDescriptor;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+
 
 import es.ulpgc.eite.clean.mvp.GenericActivity;
 import es.ulpgc.eite.clean.mvp.sample.R;
 import es.ulpgc.eite.clean.mvp.sample.app.Shop;
-import es.ulpgc.eite.clean.mvp.sample.calendar.CalendarView;
-import es.ulpgc.eite.clean.mvp.sample.chat.ChatView;
-import es.ulpgc.eite.clean.mvp.sample.home.HomePresenter;
-import es.ulpgc.eite.clean.mvp.sample.home.HomeView;
-import es.ulpgc.eite.clean.mvp.sample.webshop.WebshopView;
+
 
 public class MapsView
     extends GenericActivity<Maps.PresenterToView, Maps.ViewToPresenter, MapsPresenter>
